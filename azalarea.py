d16c18f9b3e1686100f8efe75d2f9f8190090473
Prajna Prasetya <31295104+cptdeka10@users.noreply.github.com>
2018-12-18 11:25:08 +0700

    Update azalarea.py

--- azalarea.py
+++ azalarea.py
@@ -1,38 +1,60 @@
-import requests
-from time import sleep
+import os
+import logging
 
-url = "https://api.telegram.org/bot<token>/"
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram.ext
 
-def get_updates_json(request):
-    params = {'timeout': 100, 'offset': None}
-    response = requests.get(request + 'getUpdates', data=params)
-    return response.json()
+from datetime import datetime, timedelta
+from pytz import timezone
+import pytz
 
-def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
 
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
+# add responses
+def start(bot, update):
+    update.effective_message.reply_text("Um... Let's do our best......today.", quote=False)
+    
+def caps(bot, update,args):
+    text_caps = ' '.join(args).upper()
+    update.effective_message.reply_text(text_caps)
 
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+def bio(bot, update):
+    update.effective_message.reply_text("Name       : 	望月杏奈 (Mochizuki Anna) \nBirthday   : 	May 31st \nBlood type: 	AB \nHobbies    : 	Online games \nSkill      : 	Typing \nLikes      : 	Cute things", quote=False)
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
+def ping (bot, update):
+    update.effective_message.reply_text("Um... I'm...OK.", quote=False)
 
-send_mess(chat_id, 'Your message goes here')
+def time (bot, update,args):
+    args_caps = ' '.join(args).upper()
+    target = timezone(args_caps)
+    target_tz = target.localize(telegram.Message.date)
+    update.effective_message.reply_text("The date and time in ", args, " is ", target_tz)
 
-def main():
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)       
+# add handlers
+start_handler = CommandHandler('start', start)
+caps_handler = CommandHandler('caps', caps, pass_args=True)
+bio_handler = CommandHandler('bio', bio)
+ping_handler = CommandHandler('ping', ping)
+time_handler = CommandHandler('time', time, pass_args=True)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    NAME = "AnnaMochizukiBot"
+    TOKEN = os.environ['TOKEN']
+    
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    
+    PORT = os.environ.get('PORT')
+    updater = Updater(TOKEN)
+    
+    dp = updater.dispatcher
+    dp.add_handler(start_handler)
+    dp.add_handler(caps_handler)
+    dp.add_handler(bio_handler)
+    dp.add_handler(ping_handler)
+    dp.add_handler(time_handler)
+    
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+updater.idle()
